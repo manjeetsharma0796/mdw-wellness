a8d4f377f81d50ef453e307f25f365fdf73fb2cd
manjeet0796 <manjeetsharma0796@gmail.com>
2026-06-03 21:48:59 +0530

    why-choose-us: switch to 7-in-row at lg (single row layout)

--- src/components/why-choose-us.tsx
+++ src/components/why-choose-us.tsx
@@ -19,7 +19,7 @@ export function WhyChooseUs() {
           </p>
         </div>
 
-        <div className="mt-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="mt-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7">
           {whyChooseUsItems.map((item) => (
             <div
               key={item.id}
